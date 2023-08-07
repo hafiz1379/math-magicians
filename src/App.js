3bf959caf25359978384b3ff4926c3bc693da103
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-08 00:01:00 +0430

    Updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculatoe';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
-
-export default App;
