2049355af82f604c4777f96f09a23b67fc5038dd
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-09 19:01:08 +0430

    Imported the quote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './Quote/quote';
 
 export default function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
