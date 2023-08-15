1a7ae1b86d6ad228a64cf540561ecfc35ace4749
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-15 16:19:14 +0430

    Imported all pages into nav

---
+++ src/home/navigation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import './fullwebsite.css';
+import Home from './home';
+import CalculatorPage from './calculator';
+import Quote from './quote';
+
+function Navigation() {
+  return (
+    <nav>
+      <h1 className="head-name" style={{ fontSize: '35px' }}>
+        {' '}
+        Math-magicians
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <p>|</p>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <p>|</p>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </nav>
+  );
+}
+
+export default Navigation;
