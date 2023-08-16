e8a7dcfd7ca82efac25566160f79ed12921f0051
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-16 23:22:01 +0430

    Solved linter errors

---
+++ src/home/calculatorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+function CalculatorPage() {
+  return (
+    <div className="calculatorPage">
+      <h1>Lets do some math!</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default CalculatorPage;
---
+++ src/home/quotePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuoteComponent from '../QuoteComponent/quote';
+
+function Quote() {
+  return (
+    <div className="quotePage">
+      <QuoteComponent />
+    </div>
+  );
+}
+
+export default Quote;
